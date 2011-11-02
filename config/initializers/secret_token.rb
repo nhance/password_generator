d419486e8b968574cdb479871ca97d06fdad66d9
Reenhanced LLC <github@reenhanced.com>
2011-11-02 19:23:36 -0400

    Base rails 3.1, rspec, mongoid, cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasswordGenerator::Application.config.secret_token = 'fe6652c2d24b8f53536643bfaa2a86c35742b451f8371d4b938dda9a032a4d2ed4a7827554b35ff5d2976092e865902fdcc76cd2c1592913ffc87f49437b99f5'
